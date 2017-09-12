696707e1227872951dae6ec5ecb84a3021063495
Youssef Ali <youssefali6212@gmail.com>
2017-09-12 15:43:17 +0300

    Add files via upload

---
+++ Learning Java Projects/06 - ProgrammingLanguages/src/programminglanguages/ProgrammingLanguages.java
@@ -0,0 +1,24 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package programminglanguages;
+import javax.swing.*;
+/**
+ *
+ * @author Peggy Fisher
+ * @author Youssef Ali
+ */
+public class ProgrammingLanguages {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+       JFrame jf = new LanguagesUI();
+       jf.setTitle("Programming Languages");
+       jf.setVisible(true);
+    }
+    
+}
